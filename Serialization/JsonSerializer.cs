465a14cfd8ad5578aeb0ca972e024bec0c10b694
Sean Kennealy <spkennealy@gmail.com>
2020-01-08 20:19:50 -0800

    Amazon serialization ToJson method

--- Serialization/JsonSerializer.cs
+++ Serialization/JsonSerializer.cs
@@ -1,7 +1,22 @@
-﻿namespace Serialization
+﻿using System.Text;
+using System.IO;
+using Amazon.Lambda.Serialization.Json;
+using System.Xml.Serialization;
+using System;
+
+namespace Serialization
 {
     public static class JsonSerializer
     {
-            
+        private static readonly JsonSerializer jsonSerializer = new JsonSerializer();
+
+        public static string ToJson<T>(T input)
+        {
+            using (var ms = new MemoryStream())
+            {
+                jsonSerializer.Serialize(input, ms);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
     }
 }
